9dd5c375b3a1ea3b6ddaf09b26ed630a833eab0b
Igor Iugin <gigorsaurus@gmail.com>
2023-02-08 23:04:51 +0300

    Add svg & file loaders

--- config/build/loaders.ts
+++ config/build/loaders.ts
@@ -10,19 +10,34 @@ export function buildLoaders({isDev}: BuildOptions): RuleSetRule[] {
 		exclude: /node_modules/
 	}
 
+	const svg = {
+		test: /\.svg$/i,
+		issuer: /\.[jt]sx?$/,
+		use: ['@svgr/webpack']
+	}
+
+	const files = {
+		test: /\.(png|jpe?g|webp|avif)$/i,
+		use: [
+			{
+				loader: 'file-loader'
+			}
+		]
+	}
+
 	const css = {
 		test: /\.s[ac]ss$/i,
 		use: [
 			isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
 			{
-				loader:'css-loader',
+				loader: 'css-loader',
 				options: {
 					modules: {
 						auto: /\.m./i,
 						localIdentName: isDev
 							? '[name]__[local]--[hash:base64:5]'
-							: '[hash:base64:5]',
-					},
+							: '[hash:base64:5]'
+					}
 				}
 			},
 			'sass-loader'
@@ -30,6 +45,8 @@ export function buildLoaders({isDev}: BuildOptions): RuleSetRule[] {
 	}
 
 	return [
+		files,
+		svg,
 		typescript,
 		css
 	]
--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -2,6 +2,13 @@ declare module '*.scss' {
 	interface IClassNames {
 		[className: string]: string
 	}
+
 	const classNames: IClassNames
 	export = classNames
+}
+
+declare module '*.svg' {
+	import React from 'react'
+	const content: React.FC<React.SVGAttributes<SVGElement>>
+	export default content
 }
