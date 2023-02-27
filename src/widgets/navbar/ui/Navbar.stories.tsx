c876b6b7e3986a816eec5eb44712ac52b45da22e
Igor Iugin <gigorsaurus@gmail.com>
2023-02-27 21:41:19 +0300

    Made Navbar stories

---
+++ src/widgets/navbar/ui/Navbar.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import type {ComponentMeta, ComponentStory} from '@storybook/react'
+
+import {Navbar} from './Navbar'
+import {ThemeDecorator} from 'shared/config/storybook'
+
+
+export default {
+	title: 'widget/Navbar',
+	component: Navbar,
+	argTypes: {
+		backgroundColor: {control: 'color'}
+	}
+} as ComponentMeta<typeof Navbar>
+
+const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />
+
+export const Light = Template.bind({})
+Light.args = {}
+
+export const Dark = Template.bind({})
+Dark.args = {}
+Dark.decorators = [ThemeDecorator('dark')]
