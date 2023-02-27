42b2ec745a08f8583ede5dbb4aae9d39e877e1d8
Igor Iugin <gigorsaurus@gmail.com>
2023-02-27 20:21:21 +0300

    Install storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,33 +4,26 @@ module.exports = {
 		es2021: true,
 		jest: true
 	},
-	extends: [
-		'plugin:react/recommended',
-		'standard-with-typescript',
-		'plugin:i18next/recommended'
-	],
+	extends: ['plugin:react/recommended', 'standard-with-typescript', 'plugin:i18next/recommended', 'plugin:storybook/recommended'],
 	overrides: [],
 	parserOptions: {
 		ecmaVersion: 'latest',
 		sourceType: 'module',
 		project: 'tsconfig.json'
 	},
-	plugins: [
-		'react',
-		'@typescript-eslint',
-		'i18next'
-	],
+	plugins: ['react', '@typescript-eslint', 'i18next'],
 	rules: {
 		'no-multiple-empty-lines': 'off',
 		quotes: 1,
 		indent: ['error', 'tab'],
-		'no-tabs': ['error', {allowIndentationTabs: true}],
+		'no-tabs': ['error', {
+			allowIndentationTabs: true
+		}],
 		'object-curly-spacing': ['error', 'never'],
 		'react/react-in-jsx-scope': 0,
 		'no-trailing-spaces': [2, {
 			skipBlankLines: true
 		}],
-
 		'@typescript-eslint/indent': ['error', 'tab'],
 		'@typescript-eslint/object-curly-spacing': 'off',
 		'@typescript-eslint/consistent-type-imports': [2, {
@@ -43,7 +36,6 @@ module.exports = {
 		'@typescript-eslint/no-misused-promises': 0,
 		'@typescript-eslint/no-unused-vars': 1,
 		'@typescript-eslint/ban-ts-comment': 0,
-
 		'i18next/no-literal-string': [2, {
 			markupOnly: true,
 			ignoreAttribute: ['data-testid', 'to']
