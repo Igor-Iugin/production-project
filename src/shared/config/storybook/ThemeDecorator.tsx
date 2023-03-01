2a2a9c18ad2ef0e92fcb160a6ab6b042162ea2a9
Igor Iugin <gigorsaurus@gmail.com>
2023-03-01 21:01:48 +0300

    Fix display-name error

--- src/shared/config/storybook/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator.tsx
@@ -3,6 +3,7 @@ import type {Story} from '@storybook/react'
 import type {Theme} from 'shared/contexts/ThemeContext/ThemeProvider'
 
 
+// eslint-disable-next-line react/display-name
 export const ThemeDecorator = (theme: Theme, grid: boolean = false) => (StoryComponent: Story) => (
 	<div className={`app ${theme} sb ${grid ? '' : 'griddles'}`}>
 		<StoryComponent/>
